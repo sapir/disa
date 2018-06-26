cd156a600d0d2297014a6ae949ac6bbe3d7d49f6
Y. Sapir <yasapir@gmail.com>
2018-06-27 02:19:41 +0300

    Include instruction addresses in AvrDisassembler output.

--- src/avr_disa.rs
+++ src/avr_disa.rs
@@ -2,22 +2,25 @@ use avr_insn::*;
 
 
 pub struct AvrDisassembler<'a> {
+    addr: u32,
     input: &'a [u16],
 }
 
 impl<'a> AvrDisassembler<'a> {
-    pub fn new(input: &'a [u16]) -> Self {
-        AvrDisassembler { input }
+    pub fn new(addr: u32, input: &'a [u16]) -> Self {
+        AvrDisassembler { addr, input }
     }
 }
 
 impl<'a> Iterator for AvrDisassembler<'a> {
-    type Item = AvrInsn;
+    type Item = (u32, AvrInsn);
 
-    fn next(&mut self) -> Option<AvrInsn> {
+    fn next(&mut self) -> Option<(u32, AvrInsn)> {
         AvrInsn::decode(self.input).map(|(rest, insn)| {
             self.input = rest;
-            insn
+            let ret = (self.addr, insn);
+            self.addr += insn.byte_size() as u32;
+            ret
         })
     }
 
